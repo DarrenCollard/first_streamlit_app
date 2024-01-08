2e8cd27f17bb1bd6f535b2bb726d3bf7c3f917e7
Darren Collard <darrencollard@hotmail.com>
2024-01-08 16:39:04 -0500

    select box

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwifruit')
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwifruit')
+fruit_choice = streamlit.selectbox("Pick some fruits:", list(my_fruit_list.index))
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # convert .json info to data frame
