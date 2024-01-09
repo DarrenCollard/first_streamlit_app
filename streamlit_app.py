53afdbe83dcd0fddbbc9f8357ac6226a09327347
Darren Collard <darrencollard@hotmail.com>
2024-01-08 23:06:31 -0500

    change option def

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwifruit')
-fruit_choice = streamlit.selectbox("What Fruit would you like information about?", list(my_fruit_list.index), 'Apple')
+fruit_choice = streamlit.selectbox("What Fruit would you like information about?", list(my_fruit_list.index), option=1)
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # convert .json info to data frame
